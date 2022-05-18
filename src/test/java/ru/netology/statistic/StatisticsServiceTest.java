e2b639bc8004e6d98309685e43d58c1a247c3eff
Азат Ильдарханов <defcon797@gmail.com>
2022-05-18 21:19:07 +0300

    In StatisticsServiceTest.java: 1. Returned array in findMax defaul configuration 2. Add new test shouldFindMaxInEnd

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -10,8 +10,19 @@ public class StatisticsServiceTest {
     void findMax() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {9, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldFindMaxInEnd() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {1, 3, 7, 3, 9, 10, 1, 3, 6, 5, 15};
+        long expected = 15;
 
         long actual = service.findMax(incomesInBillions);
 
